b91627b60806fa0db1740101b7dc524e80e4c713
aboulie <aboulie@knights.ucf.edu>
2018-09-21 22:04:48 -0400

    final

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">© 2018 Dareen Mohamad</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import pica from './images/pica.jpg';
+import picb from './images/picb.png';
+import picc from './images/picc.jpg';
+import picd from './images/picd.jpg';
+import pice from './images/pice.jpg';
+import picf from './images/picf.jpg';
+import picg from './images/picg.jpg';
+import pich from './images/pich.jpg';
+import pici from './images/pici.png';
+import picj from './images/picj.jpg';
+import pick from './images/pick.png';
+import picl from './images/picl.jpg';
+
+const images = [
+    pica,
+    picb,
+    picc,
+    picd,
+    pice,
+    picf,
+    picg,
+    pich,
+    pici,
+    picj,
+    pick,
+    picl
+];
+
+export default images;
